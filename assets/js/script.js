d3ff6a05cb1efeba5261dca2e09859e7323e6399
ethanfrog <101209254+ethanwynne395@users.noreply.github.com>
2023-11-12 18:57:35 -0500

    Searching a city now creates a new search button for that city

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,34 @@
-$('#search-button').on('click', function () {
-    console.log("Search button clicked");
-});
+var searchBar = $('#search-bar');
+var searchButton = $('#search-button');
+
+var savedSearchDiv = $('#saved-searches');
+
+searchButton.on('click', showClick);
+
+function showClick() {
+    var searchQuery = searchBar.val();
+    // Save new query to local storage
+    saveToLocalStorage(searchQuery);
+    // Create new query button
+    newCityButton(searchQuery);
+}
+
+//Create new query button
+function newCityButton(cityName) {
+    console.log(cityName);
+
+    var newButton = $('<button>');
+    newButton.html(cityName);
+
+    savedSearchDiv.append(newButton);
+}
+
+//Save new query to local storage
+function saveToLocalStorage(cityName) {
+
+}
+
+//Create buttons from local storage
+function createSavedButtons() {
+
+}
