7d73ca1d1555c3beecbf570085c017077d608def
ethanfrog <101209254+ethanwynne395@users.noreply.github.com>
2023-11-13 17:50:08 -0500

    Added a call to the OpenWeather API

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,25 @@ var searchButton = $('#search-button');
 
 var savedSearchDiv = $('#saved-searches');
 
-searchButton.on('click', showClick);
-
 createSavedButtons();
 
-function showClick() {
+searchButton.on('click', searchForCity);
+
+//Create buttons from local storage
+function createSavedButtons() {
+    for (i = 0; i < localStorage.length; i++) {
+        newCityButton(localStorage.key(i));
+    }
+}
+
+function searchForCity() {
     var searchQuery = searchBar.val();
     // Save new query to local storage
     saveToLocalStorage(searchQuery);
     // Create new query button
     newCityButton(searchQuery);
+    // Get the weather of the given location
+    getWeather(searchQuery);
 }
 
 //Create new query button
@@ -30,9 +39,19 @@ function saveToLocalStorage(cityName) {
     localStorage.setItem(cityName, JSON.stringify(" "));
 }
 
-//Create buttons from local storage
-function createSavedButtons() {
-    for (i = 0; i < localStorage.length; i++) {
-        newCityButton(localStorage.key(i));
-    }
+function getWeather(cityName) {
+    var city = cityName;
+    var key = "c7cd1d281114a995fffcd325175b8a5e";
+
+    var url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + key + "&per_page=5";
+
+    console.log(url);
+
+    fetch(url)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        })
 }
